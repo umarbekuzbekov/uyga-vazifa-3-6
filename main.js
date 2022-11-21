ccd2b83a93bb8e1a98724dcf76bd6a66233621df
umarbekuzbekov <umarbekuzbekov1611@gmail.com>
2022-11-21 13:06:01 +0200

    moshina obyekti yasaldi

---
+++ main.js
@@ -0,0 +1,30 @@
+const moshina={
+    nomi:'Nissan GTR',
+    boshZavodi:'Yaponiyada',
+    chiqariladi:{
+        asosiysi:"Yaponiyada",
+        ikinchi:"AQSH",
+    },
+asosiyBozor:{
+    birinchi:"Aqsh hamda Rossiya",
+ketingilari:"Yevropa davlat;ari"
+},
+
+ishlabchiqarigan:{
+    dastlab:1998,
+    keyin:2005,
+    oxirgisi:2022,
+},
+
+otKuchi:"1400 gacha",
+mazimal:"3400 km/s",
+rekordi:"eng tez va uzoq dirift",
+narxiYaponiyada:"$85.000",
+narxiUzbekistonda:"$135.000",
+xususiyati:"tezkor Sport car"
+}
+console.log(moshina);
+
+
+
+
